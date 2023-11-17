76ce9df42b385be7e93a1de3769b1c15cde13df9
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-11-17 23:24:12 +0900

    first commit GameModebase, playerCharacter, Ghost class/bp 추가 Ghost에 Camera, SpringArm 컴포넌트 추가

---
+++ Source/Study/GameInfo.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+
+
+#define UPROPERTY_EDITANY_BPRW_APC_TRUE	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+
+
+
+/**
+ * 
+ */
+class STUDY_API GameInfo
+{
+public:
+	GameInfo();
+	~GameInfo();
+};
---
+++ Source/Study/Player/Ghost.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PlayerCharacter.h"
+#include "Ghost.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDY_API AGhost : public APlayerCharacter
+{
+	GENERATED_BODY()
+	
+};
