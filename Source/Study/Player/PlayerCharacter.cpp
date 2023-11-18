62316bd1dee170ef57f864c25bf5a64d75041048
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-11-19 01:56:16 +0900

    [추가] -Asset IA_Movement : 이동을 담당하는 InputAction 에셋 IMC_BasicPlayer : 모든 플레이어 캐릭터가 사용할 Input Mapping Context
    -BluePrint
    
    -Cpp
    
    -기타
    
    [수정]
    -Asset
    
    -BluePrint
    BPGhost : 위에 추가한 IA, IMC를 EnhancedInput으로 처리하도록 함 (cpp에서 바인딩 되게 해야함)
    BPMyGameMode : 시작 맵 Main으로 되도록 설정
    
    -Cpp
    APlayerCharacter : SetupPlayerInputComponent 함수 내용 채움
    InputMappingContext 추가 및 액션 바인딩 수행
    
    UInputMappingContext, UInputAction BP에서 접근 가능하게 추가
    
    -기타
    gitingore : MyContents내 모든 하위폴더의 uasset파일 올라가도록 수정
    
    [삭제]
    -Asset
    
    -BluePrint
    
    -Cpp
    
    -기타

--- Source/Study/Player/PlayerCharacter.cpp
+++ Source/Study/Player/PlayerCharacter.cpp
@@ -2,9 +2,17 @@
 
 
 #include "PlayerCharacter.h"
+//#include "MyEnhancedInputComponent.generated.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
+	: mCamera(nullptr)
+	, mSpringArm(nullptr)
+	, mInputMappingContext(nullptr)
+	, mAction_Move(nullptr)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,13 +20,18 @@ APlayerCharacter::APlayerCharacter()
 	mCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	mSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Arm"));
 
+
+	mCamera->SetupAttachment(mSpringArm);
+
+	mSpringArm->TargetArmLength = 500.f;
+	mSpringArm->TargetOffset = { 0.f, 0.f, 100.f };
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -32,6 +45,31 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Get the player controller
+	auto playerController = Cast<APlayerController>(GetController());
+
+	// Get the local player enhanced input subsystem
+	auto eiSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+	//Add the input mapping context
+	eiSubsystem->AddMappingContext(mInputMappingContext, 0);
+
+	// Get the EnhancedInputComponent
+	auto playerEIcomponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	//Bind Move() to the mapping
+	//BindAction for enhanced system takes Action, ETriggerEvent, object, and function
+	//ETriggerEvent is an enum, where Triggered means "button is held down".
+	playerEIcomponent->BindAction(mAction_Move, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+}
+
+void APlayerCharacter::Move(const FInputActionValue& value)
+{
+	const FVector3d moveVector = value.Get<FVector3d>();
+
+	AddMovementInput({ 1.f, 0.f, 0.f }, 10.f);
+
+	UE_LOG(LogTemp, Warning, TEXT("Test log"));
 
 }
 
--- Source/Study/Player/PlayerCharacter.h
+++ Source/Study/Player/PlayerCharacter.h
@@ -22,9 +22,17 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* mSpringArm;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* mInputMappingContext;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category ="Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* mAction_Move;
+
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
 
 public:	
 	// Called every frame
@@ -32,5 +40,7 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Move(const struct FInputActionValue& value);
 
 };
